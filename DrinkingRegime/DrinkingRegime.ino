4bc0393b306aa28b96f490b58b12cea8c29db52d
Keplen <Zbynek.poskocil@gmail.com>
2018-06-09 09:06:08 +0200

    Initial commit.

---
+++ DrinkingRegime/DrinkingRegime.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		DrinkingRegime.ino
+ Created:	6/9/2018 9:02:13 AM
+ Author:	Zbynek
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
